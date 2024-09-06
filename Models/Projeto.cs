83626092b632ad9554515c18919fd97aad251e46
Felipe Ribeiro <felipedejesusifgoiano@gmail.com>
2024-09-06 17:57:55 -0300

    Alterando para o padrão MVVM.

---
+++ Models/Projeto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace WpfImportExport.Models
+{
+    internal class Projeto
+    {
+
+        public string projectPath { get; set; }
+
+
+        public void ProcurarProjeto()
+        {
+
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog.Filter = "All Files (*.*)|*.*"; // You can set your own filter here
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // The user selected a file, and you can get the file path
+                string filePath = openFileDialog.FileName;
+                // You can perform further actions with the file here
+                projectPath = filePath;
+            }
+
+        }
+    }
+}
---
+++ ViewModels/TraceWriterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfImportExport.Interfaces;
+
+namespace WpfImportExport.ViewModels
+{
+    internal class TraceWriterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TraceWriter _traceWriter {  get; set; } 
+
+        public void NotifyPropertyChanged(string propertyName = null) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
