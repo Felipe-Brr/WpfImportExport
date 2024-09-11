95fb784139e5add767a6ce25db6708124e8c8527
Felipe Ribeiro <felipedejesusifgoiano@gmail.com>
2024-09-11 13:42:28 -0300

    Refatoração de UI e melhorias de rastreamento
    Refatoração significativa da interface do usuário, consolidando frames em um único frame `MainContent` e simplificando layouts. Alterações no comportamento de redimensionamento e estilo da janela. Adição de rastreamento de chamadas em métodos críticos e comandos. Criação da nova página `ImportarExportar` para centralizar funcionalidades de abrir, exportar e importar blocos. Atualizações no projeto para refletir essas mudanças.

---
+++ Views/ImportarExportar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfImportExport.Views
+{
+    /// <summary>
+    /// Interação lógica para ImportarExportar.xam
+    /// </summary>
+    public partial class ImportarExportar : Page
+    {
+        public ImportarExportar()
+        {
+            InitializeComponent();
+            abrirProjeto.Content = new AbrirProjeto();
+            exportarBloco.Content = new ExportarBloco();
+            importarBloco.Content = new ImportarBloco();
+            message.Content = new Views.Message();
+        }
+    }
+}
